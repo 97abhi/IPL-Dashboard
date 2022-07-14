a20e89c911c80d83c41d5fd1da5ad4616ccee4aa
Abhishek Mishra <97abhi@gmail.com>
2022-07-14 11:37:25 +0530

    first commit

---
+++ ipl_dashboard/src/main/java/com/example/Controller/TeamController.java
@@ -0,0 +1,32 @@
+package com.example.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Team;
+import com.example.repository.MatchRepository;
+import com.example.repository.TeamRepository;
+
+@RestController
+public class TeamController {
+
+
+    private TeamRepository teamRepository;
+    private MatchRepository matchRepository;
+
+    public TeamController(TeamRepository teamRepository, MatchRepository matchRepository){
+        this.teamRepository = teamRepository;
+        this.matchRepository = matchRepository;
+    }
+
+    @GetMapping("/team/{teamName}")
+    public Team getTeam(@PathVariable String teamName){
+
+        //PageRequest helps you to limit the pages of display. In this case it limits to 4 pages
+        
+        Team team =  this.teamRepository.findByTeamName(teamName);
+        team.setMatches(this.matchRepository.findLatestMtchesByTeam(teamName,4));
+        return team;
+    }
+}
