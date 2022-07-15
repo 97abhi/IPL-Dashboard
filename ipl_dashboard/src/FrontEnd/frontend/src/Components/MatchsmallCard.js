46e69b5b00be19f3e3621e2d255ecb97c0550957
Abhishek Mishra <97abhi@gmail.com>
2022-07-15 17:16:36 +0530

    React App with Team CHanges

---
+++ ipl_dashboard/src/FrontEnd/frontend/src/Components/MatchsmallCard.js
@@ -0,0 +1,8 @@
+import { React } from 'react';
+export const MatchsmallCard= ({match}) => {
+  return (
+    <div className="MatchsmallCard">
+     <p>{match.team1}vs{match.team2}</p>
+    </div>
+  );
+}
--- ipl_dashboard/src/main/java/com/example/Controller/TeamController.java
+++ ipl_dashboard/src/main/java/com/example/Controller/TeamController.java
@@ -1,5 +1,6 @@
 package com.example.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import com.example.repository.MatchRepository;
 import com.example.repository.TeamRepository;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
 
